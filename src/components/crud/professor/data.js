422807b65c55ee2dfa53959b96bc60ca90110a29
guilherme seixas <guilhermesantoseixas@outlook.com>
2022-05-29 16:22:34 -0300

    firebase firestore crud

---
+++ src/components/crud/professor/data.js
@@ -0,0 +1,8 @@
+const professors = [
+    {id:0, name:"Jefferson Carvalho", university: "UFC", degree:"Mestrado"},
+    {id:1, name:"Enyo Gonçalves", university: "UFC", degree:"Graduação"},
+    {id:2, name:"Livia Almada", university: "UFC", degree:"Doutorado"},
+    {id:3, name:"Atílio Gomes", university: "UFC", degree:"Mestrado"},
+]
+
+export {professors}
